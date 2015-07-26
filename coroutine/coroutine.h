41b9278a7b416b8a58a5e3490034d29f4d40cd59
YuYangZi <289633152@qq.com>
2015-07-26 10:23:45 -0700

    用户block相关宏定义，同步方面暂时有bug，稍后处理

--- coroutine/coroutine.h
+++ coroutine/coroutine.h
@@ -12,3 +12,8 @@ struct __go
 
 #define go __go()-
 #define yield do { Scheduler::getInstance().Yield(); } while (0)
+
+// co_block(uint32_t type, uint64_t id, bool yield_immediately = true);
+#define co_block(...) do { Scheduler::getInstance().UserBlockSwitch(__VA_ARGS__); } while (0)
+#define co_cancel_block() do { Scheduler::getInstance().UserBlockCancel(); } while (0)
+#define co_weak(type, id) do { Scheduler::getInstance().UserBlockWeak(type, id); } while (0)
