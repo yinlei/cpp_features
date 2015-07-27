d991f2c876611ae5b5bc6bee180785de5054989d
yuyangzi <yuyangzi@meizu.com>
2015-07-27 15:27:03 +0800

    修改宏

--- coroutine/coroutine.h
+++ coroutine/coroutine.h
@@ -13,7 +13,6 @@ struct __go
 #define go __go()-
 #define yield do { Scheduler::getInstance().Yield(); } while (0)
 
-// co_block(uint32_t type, uint64_t id, bool yield_immediately = true);
-#define co_block(...) do { Scheduler::getInstance().UserBlockSwitch(__VA_ARGS__); } while (0)
-#define co_cancel_block() do { Scheduler::getInstance().UserBlockCancel(); } while (0)
-#define co_weak(type, id) do { Scheduler::getInstance().UserBlockWeak(type, id); } while (0)
+// (uint32_t type, uint64_t id)
+#define co_wait(type, id) do { Scheduler::getInstance().UserBlockWait(type, id); } while (0)
+#define co_wakeup(type, id) do { Scheduler::getInstance().UserBlockWakeup(type, id); } while (0)
