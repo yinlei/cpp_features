94c54cda63b267d24ae90fe3970e4d86df64a6bf
yyz <289633152@qq.com>
2015-08-24 22:59:31 +0800

    adapter boost1.59

--- coroutine/win_patch/ucontext.cpp
+++ coroutine/win_patch/ucontext.cpp
@@ -7,13 +7,22 @@ extern "C" {
 void makecontext(ucontext_t *ucp, void (*func)(), int argc, void* argv)
 {
     ucp->arg = argv;
+#ifdef OLD_BOOST_CONTEXT
     ucp->native = ::boost::context::make_fcontext(ucp->uc_stack.ss_sp,
             ucp->uc_stack.ss_size, (void(*)(intptr_t))func);
+#else
+    ucp->native = ::boost::context::make_fcontext(ucp->uc_stack.ss_sp,
+            ucp->uc_stack.ss_size, (void(*)(intptr_t))func);
+#endif
 }
 
 int swapcontext(ucontext_t *oucp, ucontext_t *ucp)
 {
+#ifdef OLD_BOOST_CONTEXT
     ::boost::context::jump_fcontext(oucp->native, ucp->native, (intptr_t)ucp->arg);
+#else
+    ::boost::context::jump_fcontext(&oucp->native, ucp->native, (intptr_t)ucp->arg);
+#endif
     return 0;
 }
 
--- coroutine/win_patch/ucontext.h
+++ coroutine/win_patch/ucontext.h
@@ -1,18 +1,37 @@
 #pragma once
 
+#include <boost/version.hpp>
 #include <boost/context/all.hpp>
+
+#if (BOOST_VERSION < 105900)
+#define OLD_BOOST_CONTEXT
+#endif
+
 
 namespace co {
 
+#ifdef OLD_BOOST_CONTEXT
 typedef struct sigaltstack {                       
     void *ss_sp;          
     int ss_flags;         
     size_t ss_size;       
 } stack_t;              
+#else
+typedef struct sigaltstack {                       
+    void **ss_sp;          
+    int ss_flags;         
+    size_t ss_size;       
+} stack_t;              
+#endif
 
 struct ucontext_t
 {
+#ifdef OLD_BOOST_CONTEXT
     ::boost::context::fcontext_t *native;
+#else
+    ::boost::context::fcontext_t native;
+#endif
+
     ucontext_t* uc_link;
     stack_t uc_stack;
     void* arg;
